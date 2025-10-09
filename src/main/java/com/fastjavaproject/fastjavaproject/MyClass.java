64f2e3d450e135992870486f9b8d183f5065cbe3
Sonusgit <sonugitcom@gmail.com>
2025-10-10 00:49:42 +0530

    add fast commit endpoint /hello

--- src/main/java/com/fastjavaproject/fastjavaproject/MyClass.java
+++ src/main/java/com/fastjavaproject/fastjavaproject/MyClass.java
@@ -1,8 +1,12 @@
 package com.fastjavaproject.fastjavaproject;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyClass {
-
+  @GetMapping("/hello")
+	public String hello() {
+		return "Hello World!";
+	}
 }
